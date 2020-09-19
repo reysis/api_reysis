c28da346523ac88c922fd62901e5ce5e820a5619
Reynelbis Larquin Guerra <reynelbis.larquin@reduc.edu.cu>
2020-09-19 19:05:30 -0400

    Se agregaron los fitros para la entidad TipoUsuario

--- src/Entity/TipoUsuario.php
+++ src/Entity/TipoUsuario.php
@@ -7,9 +7,42 @@ use App\Repository\TipoUsuarioRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *  iri="http://schema.org/TipoUsuario",
+ *     collectionOperations={
+ *          "get" = {"accessControl" = "is_granted('ROLE_ADMIN')"},
+ *          "post" = {"security_post_denormalize"="is_granted('POST', object)",
+ *                  "security_post_denormalize_message"="Solo un Administrador puede crear Tipos de Servicios"
+ *          }
+ *     },
+ *     itemOperations={
+ *          "get" = {"accessControl" = "is_granted('ROLE_ADMIN')"},
+ *          "put" = {
+ *                  "security"="is_granted('EDIT', object)",
+ *                  "security_message"="Solo un Administrador puede editar Tipos de Servicios."
+ *          },
+ *          "delete" = {
+ *                  "security"="is_granted('ERASE', object)",
+ *                  "security_message"="No puede realizar esta acción a menos que sea administrador."
+ *          }
+ *      },
+ *      attributes={
+ *          "pagination_items_per_page"=10,
+ *      }
+ * )
+ * @ApiFilter(PropertyFilter::class)
+ * @ApiFilter(
+ *      SearchFilter::class,
+ *      properties={
+ *          "tipo":"partial"
+ *      }
+ * )
  * @UniqueEntity("tipo")
  * @ORM\Entity(repositoryClass=TipoUsuarioRepository::class)
  */
@@ -24,7 +57,8 @@ class TipoUsuario
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
-     * @Groups({"user:read"})
+     * @ApiProperty(iri="http://schema.org/tipo")
+     * @Groups({"user:read", "admin:write"})
      */
     private $tipo;
 
