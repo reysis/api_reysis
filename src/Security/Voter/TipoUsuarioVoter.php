b463d4033ce4d0903c6610acea27f7874e947244
Reynelbis Larquin Guerra <reynelbis.larquin@reduc.edu.cu>
2020-09-19 19:09:56 -0400

    Se realizo el voter de TipoUsuario

---
+++ src/Security/Voter/TipoUsuarioVoter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\TipoUsuario;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class TipoUsuarioVoter extends Voter
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports($attribute, $subject)
+    {
+        /* Si Modificas alguna Operaci'on, para que el Voter
+            la vea tienes que agregarla aqui por ejemplo:
+            return in_array($atribute, ['POST', 'GET_SPECIFI', 'CUALQUIER_COSA'])
+                && $subject instance of TipoUsuario;
+        */
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, ['GET','ERASE', 'EDIT', 'POST'])
+            && $subject instanceof TipoUsuario;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        /**
+         * @var TipoUsuario $subject
+         */
+        /*
+         * Aqui tienes que agregar la logica de seguridad...
+         * Si es Admin return true, sino return false... etc,
+         * un case para cada Operacion Custom que realices
+         * */
+        switch ($attribute) {
+            case 'GET':
+                if(in_array('ROLE_ADMIN', $user->getRoles()))
+                    return true;
+                return false;
+            case 'POST':
+                if(in_array('ROLE_ADMIN', $user->getRoles()))
+                    return true;
+                return false;
+            case 'EDIT':
+                if(in_array('ROLE_ADMIN', $user->getRoles()))
+                    return true;
+                return false;
+            case 'ERASE':
+                if(in_array('ROLE_ADMIN', $user->getRoles()))
+                    return true;
+                return false;
+        }
+
+        throw new \Exception(sprint('Atributo no manejado "%s" ', $attribute));
+    }
+}
---
+++ tests/Functional/TipoUsuarioResourceTest.php
@@ -0,0 +1,188 @@
+<?php
+
+
+namespace App\Tests\Functional;
+
+
+use App\Entity\User;
+use App\Test\CustomApiTestCase;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
+
+class TipoUsuarioResourceTest extends CustomApiTestCase
+{
+    use ReloadDatabaseTrait;
+
+    public function testCreateTipoServicio()
+    {
+        $client = self::createClient();
+        $tipo1 = $this->createTipoUsuario('Persona Natural');
+        $user = $this->createUser('testUser1', 'foo', '123456789', $tipo1);
+
+        $client->request('POST', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 1',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ]
+        ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        //Creando el Tipo de Servicio como Admin
+        $em = $this->getEntityManager();
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+
+        $this->logIn($client, 'testUser1', 'foo');
+        $client->request('POST', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 1',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ]
+        ]);
+        $this->assertResponseStatusCodeSame(201);
+    }
+
+    public function testGetTipoUsuario()
+    {
+        $client = self::createClient();
+
+        $client->request('GET', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+        ]);
+
+        $this->assertResponseStatusCodeSame(200);
+
+        $tipo1 = $this->createTipoUsuario('Persona Natural');
+        $user = $this->createUser('testUser1', 'foo', '123456789', $tipo1);
+
+        //Refrescando el usuario y dandole permisos de administración
+        $em = $this->getEntityManager();
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+
+        //Reloguenado para que Symfony note los permisos de administración
+        $this->logIn($client, 'testUser1', 'foo');
+
+        $client->request('GET', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+        ]);
+
+        $this->assertResponseStatusCodeSame(200);
+
+    }
+
+    public function testUpdateTipoServicio(){
+        $client = self::createClient();
+
+        $tipo1 = $this->createTipoUsuario('Persona Natural');
+        $user = $this->createUser('testUser1', 'foo', '123456789', $tipo1);
+
+        $client->request('POST', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 1',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ]
+        ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        //Creando el Tipo de Servicio como Admin
+        $em = $this->getEntityManager();
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+
+        $this->logIn($client, 'testUser1', 'foo');
+
+        $client->request('POST', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 1',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ]
+        ]);
+
+        $this->assertResponseStatusCodeSame(201);
+
+        $client->request('PUT', '/api/tipos_servicios/1', [
+            'headers'=> ['ContentType'=>'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 2',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $this->logOut($client);
+
+        $client->request('PUT', '/api/tipos_servicios/1', [
+            'headers'=> ['ContentType'=>'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 3',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(401);
+
+    }
+
+    public function testDeleteTipoServicio(){
+        $client = self::createClient();
+
+        $tipo1 = $this->createTipoUsuario('Persona Natural');
+        $user = $this->createUser('testUser1', 'foo', '123456789', $tipo1);
+
+        $client->request('POST', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 1',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ]
+        ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        //Creando el Tipo de Servicio como Admin
+        $em = $this->getEntityManager();
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+
+        $this->logIn($client, 'testUser1', 'foo');
+
+        $client->request('POST', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 1',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ]
+        ]);
+
+        $this->assertResponseStatusCodeSame(201);
+
+        $client->request('DELETE', '/api/tipos_servicios/1',[
+            'headers'=> ['ContentType'=>'application/json+ld'],
+        ]);
+
+        $this->assertResponseStatusCodeSame(204);
+
+        $client->request('POST', '/api/tipos_servicios', [
+            'headers' => ['ContentType' => 'application/json+ld'],
+            'json' => [
+                'nombre' => 'Servicio 2',
+                'descripcion' => 'Esto es una pequena descripcion',
+            ]
+        ]);
+
+        $this->logOut($client);
+
+        $client->request('DELETE', '/api/tipos_servicios/2',[
+            'headers'=> ['ContentType'=>'application/json+ld'],
+        ]);
+
+        $this->assertResponseStatusCodeSame(401);
+    }
+
+}
