1b44b205ec20c1c393ba7683cd9d8280422ec586
Reynelbis Larquin Guerra <reynelbis.larquin@reduc.edu.cu>
2020-09-19 21:46:03 -0400

    se incorporo la propiedad ApiProperty

--- src/Entity/PreguntaFrecuente.php
+++ src/Entity/PreguntaFrecuente.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PreguntaFrecuenteRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
