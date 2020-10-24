1ebbc7c38eddbf178ecd5555bfc86d67ab225f0e
FrankSiret <frank.siret@gmail.com>
2020-10-24 04:46:28 -0400

    login remasterizado

--- assets/components/user/Login.js
+++ assets/components/user/Login.js
@@ -2,10 +2,10 @@ import React, { Component, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { login } from "../../actions/user/authentication";
 import { connect } from 'react-redux';
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import { reset } from "../../actions/turno/create";
 
-import { Button, Form, InputGroup, FormLabel } from "react-bootstrap";
+import { Button, Form, InputGroup, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
@@ -53,37 +53,52 @@ class Login extends Component {
                         {this.props.error}
                     </div>
                 )}
-                <Form onSubmit={this.handleSubmit} className="form-login">
-                    <div className="header">
+                <Form onSubmit={this.handleSubmit} className="form-login" >
+                    {/* <div className="header">
                         <h1><span>Iniciar sesión</span></h1>
-                    </div>
+                    </div> */}
+                    <Form.Group className="form-header form-in-center">
+                        <h3>Iniciar Sesión</h3>
+                        <p>Entre sus datos para iniciar sessión</p>
+                    </Form.Group>
+
                     <Form.Group>
-                        <Form.Label>Usuario</Form.Label>
+                        {/* <Form.Label>Usuario</Form.Label> */}
                         <InputGroup>
                             <InputGroup.Prepend>
-                                <label for="username" >
+                                <label className="input-group-text" for="username" >
                                     <FontAwesomeIcon icon={faUser} />
                                 </label>
                             </InputGroup.Prepend>
                             <Form.Control id='username' type="user" placeholder="Usuario" onChange={this.handleChange} />
                         </InputGroup>
                     </Form.Group>
-                    {/* <Form.Group> */}
-                    {/* <Form.Label>Password</Form.Label> */}
-                    <InputGroup>
-                        <InputGroup.Prepend>
-                            <label className="input-group-text" for="password">
-                                <FontAwesomeIcon icon={faLock} />
-                            </label>
-                        </InputGroup.Prepend>
-                        <Form.Control id="password" type="password" placeholder="Contraseña" onChange={this.handleChange}></Form.Control>
-                    </InputGroup>
-                    {/* </Form.Group> */}
+
                     <Form.Group>
-                        <Button variant="primary" type="submit">Entrar</Button>
+                        {/* <Form.Label>Password</Form.Label> */}
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                                <label className="input-group-text" for="password">
+                                    <FontAwesomeIcon icon={faLock} />
+                                </label>
+                            </InputGroup.Prepend>
+                            <Form.Control id="password" type="password" placeholder="Contraseña" onChange={this.handleChange}></Form.Control>
+                        </InputGroup>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Button variant="primary" block >Entrar</Button>
+                    </Form.Group>
+
+                    <Form.Group className="form-in-center">
+                        <p className="my-2 text-left">¿Olvidaste tu contraseña?, click <a href="#">aquí</a></p>
+                        <div className="mb-3 mx-4 login-separator">
+                            <p className="my-2 hr">o</p>
+                            <Link to="/register" className="card-link">Regístrese</Link>
+                        </div>
                     </Form.Group>
                 </Form>
-            </div>
+            </div >
         );
     }
 }
