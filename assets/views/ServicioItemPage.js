c92c222c2cbfe756203efebda070a194ce648b9b
FrankSiret <frank.siret@gmail.com>
2020-12-06 13:44:35 -0600

    serv

--- assets/views/ServicioItemPage.js
+++ assets/views/ServicioItemPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ServicioItem from "../components/user/ServicioItem";
+import ServicioItem from "../components/ServicioItem";
 import { Row } from 'react-bootstrap';
 
 const ServicioItemPage = () => {
