9c279c26d535849036e67d736a90a25ce1828ef1
FrankSiret <frank.siret@gmail.com>
2021-01-20 16:23:02 -0500

    redux devtools

--- assets/store.js
+++ assets/store.js
@@ -9,12 +9,19 @@ const middleware = [routerMiddleware(history), thunk];
 
 import rootReducers from './redux/rootReducers';
 
-const store = createStore(
-	rootReducers,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = process.env.NODE_ENV === 'production'
+	? createStore(
+		rootReducers,
+		compose(
+			applyMiddleware(...middleware),
+			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		)
 	)
-);
+	: createStore(
+		rootReducers,
+		compose(
+			applyMiddleware(...middleware)
+		)
+	);
 
 export default store;
