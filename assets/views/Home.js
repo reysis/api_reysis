5585b38b59f34dc9848294b878babf54f4127d18
Customer <u861-z9udxt44uvyj@giowm1080.siteground.biz>
2021-01-20 14:18:00 +0000

    fixed directly in the server

--- assets/views/Home.js
+++ assets/views/Home.js
@@ -3,7 +3,7 @@ import Landing from '../components/Landing';
 import About from '../components/About';
 import ServicesHome from '../components/ServicesHome';
 import Estadisticas from '../components/Estadisticas';
-import Whyus from '../components/Whyus';
+import Whyus from '../components/WhyUs';
 import Opinions from '../components/Opinions';
 import Contact from '../components/Contact';
 
